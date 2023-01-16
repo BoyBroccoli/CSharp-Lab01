57915027c5098ae6de44f7fb1f2e81e5cf21e55f
Dawan Savage Bell <savagebell00@gmail.com>
2023-01-16 16:02:03 -0400

    Challenge03 Solution

---
+++ Module01/Challenge03/Challenge03/Challenge03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Challenge03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loopCount = 1;
+            while (loopCount <= 5)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+                loopCount++;
+            }
+        }
+    }
+}
