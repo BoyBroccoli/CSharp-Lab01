06fdc35de2435bb2de1ce17dbb411c1cb9ac7db8
Dawan Savage Bell <savagebell00@gmail.com>
2023-01-23 14:53:15 -0400

    updated challenge04

---
+++ Module01/Challenge04/Challenge04/Challenge04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Challenge04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int firstPosition = 0;
+            int secondPosition = 1;
+
+            Console.WriteLine("How many students will you like to enter?");
+
+            int studentCount = int.Parse(Console.ReadLine());
+
+            // creating 2d array
+
+            var studentsAndGrades = new string[studentCount, 2];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Please Enter The Student Name: ");
+                studentsAndGrades[i, firstPosition] = Console.ReadLine();
+
+                Console.WriteLine("Please Enther The Student Grade: ");
+                studentsAndGrades[i, secondPosition] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine($"Name: {studentsAndGrades[i, firstPosition]}, Grade: {studentsAndGrades[i, secondPosition]}");
+            }
+            
+        }
+    }
+}
